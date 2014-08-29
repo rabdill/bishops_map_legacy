a0698b1c77e8b433802b905cdba478e9ffdd9c6f
Rich Abdill <richard.abdill@gmail.com>
2014-08-28 20:25:51 -0400

    Added LOOK verb!

--- main.py
+++ main.py
@@ -2,24 +2,17 @@ import json
 import command
 import printer
 
-json_data = open('rooms.json')
-rooms = json.load(json_data)
-
-json_data = open('menus.json')
-menus = json.load(json_data)
+for inFile in ["rooms","menus"]:
+    json_data = open("{}.json".format(inFile))
+    vars()[inFile] = json.load(json_data)
 
 print "\n\nBishop's Map v0.1.0\n\n"
 
-current = rooms["lobby"]
-printer.room(current,rooms)
-
-next_move = command.get(current,rooms,menus)
+next_move = rooms["lobby"]
 
 while True:
     if next_move['type'] == "room":
         printer.room(next_move,rooms)
     elif next_move['type'] == "menu":
         printer.menu(next_move)
-    current = next_move
-
-    next_move = command.get(current,rooms,menus)
+    next_move = command.get(next_move,rooms,menus)
