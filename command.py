ba943fd2ff9133f506e5943f5505f5616bf5257b
Rich Abdill <richard.abdill@gmail.com>
2014-08-28 23:33:16 -0400

    Adding content, started documenting formatting

---
+++ command.py
@@ -0,0 +1,75 @@
+import printer
+
+def get(current,rooms,menus):
+    need_command = True
+    if current['type'] == "room":
+        while need_command is True:
+            command = raw_input("What do you want to do? > ").split() # makes it a list
+            # Strip out the articles:
+            try:
+                command.remove("the")
+            except Exception: 
+                pass
+            try:
+                command.remove("to")
+            except Exception: 
+                pass
+            try:
+                command.remove("a")
+            except Exception: 
+                pass
+            try:
+                command.remove("an")
+            except Exception: 
+                pass
+            try:
+                command.remove("in")
+            except Exception: 
+                pass  
+
+            # if the verb is a possible action:
+            if command[0] in current['actions']:
+                #if the noun is a possible recipient of the verb:
+                if command[1] in current['actions'][command[0]]:
+                    #if the result is a menu:
+                    if "menu" in current['actions'][command[0]][command[1]]:
+                        return menus[current['actions'][command[0]][command[1]]["menu"]]   
+
+            elif command[0] == 'go':
+                if command[1] not in current['exits']:
+                    printer.block("That's not a direction in which you can go.") 
+                else:
+                    need_command = False
+                    return rooms[current['exits'][command[1]]]
+
+            elif command[0] == 'look':
+                if command[1] not in current['exits']:
+                    printer.block("That's not a direction in which you can look.")
+                else:
+                    printer.block(rooms[current['exits'][command[1]]]['look'])
+            else:
+                printer.block("Sorry, unrecognized command.")
+
+
+    elif current['type'] == "menu":
+        while need_command is True:
+            command = raw_input("Which do you choose? > ")
+            if command in current['choices']:
+                if "final" in current['responses'][command]: #if it's not a menu
+                    printer.block(current['responses'][command]["final"])
+                    need_command = False
+                
+                #if you should get the same menu again
+                elif "loop" in current['responses'][command]:
+                    printer.block(current['responses'][command]["loop"])
+                    
+                    # If the loop changes the menu's prompt:
+                    if current['responses'][command]["prompt"] != "":
+                        current["prompt"] = current['responses'][command]["prompt"]
+                    return current
+                
+                # If it's another menu, send the next menu along:
+                else:
+                    return menus[current['responses'][command]["branch"]]
+            else:
+                printer.block("Not an option, bub.")
